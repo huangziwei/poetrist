5fd15b34829980ba3bb5878d6316fc462e649279
Ziwei Huang <huang-ziwei@outlook.com>
2025-07-14 14:15:18 +0200

    add: some files

---
+++ backup-to-mega.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+DB_SRC="./poetrist/blog.sqlite3"
+STAMP=$(date +%F_%H%M)             # e.g. 2025-07-08_0335
+REMOTE="/Root/${STAMP}.sqlite3"    # MEGA destination
+
+exec megaput --path "$REMOTE" "$DB_SRC"
+
+megals /Root/ | \
+  grep -E '\.sqlite3$' | sort | head -n -30 | \
+  while read -r old ; do
+      megarm "/Root/$old"
+  done
