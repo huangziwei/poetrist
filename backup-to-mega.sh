11c7cf6d8fcab308d4911d505eb20838611af861
Ziwei Huang <huang-ziwei@outlook.com>
2025-07-19 13:34:56 +0200

    change: backup script

--- backup-to-mega.sh
+++ backup-to-mega.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-DB_SRC="./poetrist/blog.sqlite3"
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$BASE_DIR"
+
+DB_SRC="poetrist/blog.sqlite3"
 STAMP=$(date +%F_%H%M)             # e.g. 2025-07-08_0335
 REMOTE="/Root/${STAMP}.sqlite3"    # MEGA destination
 
-exec megaput --path "$REMOTE" "$DB_SRC"
+megaput --path "$REMOTE" "$DB_SRC"
 
 megals /Root/ | \
   grep -E '\.sqlite3$' | sort | head -n -30 | \
